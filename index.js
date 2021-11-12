9f724aa599a962a926f5d60d31b11f5233ee684e
Volodymyr <gorbacovvolodimir@gmail.com>
2021-11-12 22:14:49 +0200

    add new test from loop task

--- index.js
+++ index.js
@@ -1,4 +1,86 @@
-const sum = (a, b) => a + b;
-const nativeNull = () => null;
+// const sum = (a, b) => a + b;
+// const nativeNull = () => null;
 
-module.exports = {sum, nativeNull};
+// module.exports = {sum, nativeNull};
+
+//To start testing: npm test
+
+
+// Task 1 - find sum of all number in 2 arrays
+// const arrayNums1 = [7,101,3,1,9,11,100,111] // 343
+// const arrayNums2 = [90,101,3,1,69,11,10,111] // 396
+// 343 + 396
+function sumNumsInArrays(array1, array2) {
+//   return  array1.concat(array2).reduce((sum, current) => sum + current, 0)
+    const newArr = [...array1, ...array2];
+    let sum = 0;
+    for (let i = 0; i < newArr.length; i++) {
+    sum += newArr[i]
+    }
+    return sum;
+}
+
+
+// Task 2 - Using a 'for' loop print all even
+// numbers up to and including n, as a comma separated string.
+// Don’t include 0
+// const n = 20;
+// Example output:
+// "2, 4, 6, 8, 10, 12, 14, 16, 18, 20"
+function printEvenNumbers(n) {
+ 
+}
+
+// Task 3 - Using a 'for' loop output the elements in reverse order
+// const arr = [
+//   43, "what", 9, true, "cannot", false, "be", 3, true
+// ];
+function reverseArrayItems(array) {
+    let reverse = [];
+  for (let i =  0; i < array.length; i++) {
+    reverse[i] = array[(array.length -1) - i]
+  }
+   return reverse
+}
+
+// Task 4 - Add up each element in the same position and
+// create a new array containing the sum of each pair.
+// Assume both arrays are of the same length.
+// let array1   = [4, 6, 7];
+// let array2  = [8, 1, 9];
+// Example output:
+// [12, 7, 16]
+function sumNumsSamePosition(array1, array2) {
+    let sum = array1.map((a, i) => a + array2[i]);
+    return sum;
+}
+
+// Task 5 - find sum of all number in array using 'while' loop
+function sumNums(array) {
+    let i = 0; 
+    let sum = 0;
+    while(i < array.length) {
+        sum += + array[i]; 
+        i++;
+    }
+    return sum
+}
+
+// Task 6 - filter array of string using 'for' or 'while' loop
+// and keep only elements where length > 4 and without the word 'error'
+// const strArr = ['error01', 'in a galaxy', 'start-error', far far', 'away', '0994-error']
+function filterArrayWithStrings(array) {}
+
+// Task 7 - given a number n calculate the factorial of
+// the number using 'for' or 'while' loop
+// n! => 1*2*3*4*5
+function factorial(n) {
+    let counter = 1;
+    while (n >= 1) {
+      counter *= n;
+      n--;
+    }
+    return counter;
+}
+
+module.exports = {sumNumsInArrays, printEvenNumbers, reverseArrayItems, sumNumsSamePosition, sumNums, filterArrayWithStrings, factorial};
--- index.test.js
+++ index.test.js
@@ -1,30 +1,93 @@
-const {sum, nativeNull} = require('./index');
+// const {sum, nativeNull} = require('./index');
 
-describe('Sum test', () => {
-    test('Sum should return  sum of two numbers', () => {
-        expect(sum(1, 3)).toBe(4);
-        expect(sum(1, 3)).toEqual(4);
-    });
+// describe('Sum test', () => {
+//     test('Sum should return  sum of two numbers', () => {
+//         expect(sum(1, 3)).toBe(4);
+//         expect(sum(1, 3)).toEqual(4);
+//     });
     
-    test('Sum should return value correctky comparing to other values', () => {
-        expect(sum(2, 3)).toBeGreaterThan(4);
-        expect(sum(2, 3)).toBeGreaterThanOrEqual(5);
-        expect(sum(2, 3)).toBeLessThan(14);
-        expect(sum(2, 3)).toBeLessThanOrEqual(5);
-    });
+//     test('Sum should return value correctky comparing to other values', () => {
+//         expect(sum(2, 3)).toBeGreaterThan(4);
+//         expect(sum(2, 3)).toBeGreaterThanOrEqual(5);
+//         expect(sum(2, 3)).toBeLessThan(14);
+//         expect(sum(2, 3)).toBeLessThanOrEqual(5);
+//     });
     
-    test('Sum should sum 2 float values correctly', () => {
-        expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
-    });
-})
+//     test('Sum should sum 2 float values correctly', () => {
+//         expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
+//     });
+// })
 
-describe('nativeNull function', () => {
-    test('nativeNull should return false value null', () => {
-        expect(nativeNull()).toBe(null);
-        expect(nativeNull()).toBeNull();
-        expect(nativeNull()).toBeFalsy();
-        expect(nativeNull()).toBeDefined();
-        expect(nativeNull()).not.toBeTruthy();
-        expect(nativeNull()).not.toBeUndefined();
+// describe('nativeNull function', () => {
+//     test('nativeNull should return false value null', () => {
+//         expect(nativeNull()).toBe(null);
+//         expect(nativeNull()).toBeNull();
+//         expect(nativeNull()).toBeFalsy();
+//         expect(nativeNull()).toBeDefined();
+//         expect(nativeNull()).not.toBeTruthy();
+//         expect(nativeNull()).not.toBeUndefined();
+//     });
+// });
+
+const {sumNumsInArrays, printEvenNumbers, reverseArrayItems, sumNumsSamePosition, sumNums, filterArrayWithStrings, factorial} = require('./index');
+
+describe("should test tasksWithArrayMethods", () => {
+    it("should test sumNumsInArrays", () => {
+      const arrayNums1 = [7, 101, 3, 1, 9, 11, 100, 111]; // 343
+      const arrayNums2 = [90, 101, 3, 1, 69, 11, 10, 111]; // 396
+      const output = 739;
+  
+      expect(sumNumsInArrays(arrayNums1, arrayNums2)).toEqual(output);
     });
-});
+  
+    it("should test sortArray", () => {
+      const n = 22;
+      const output = "2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22";
+  
+      expect(printEvenNumbers(n)).toEqual(output);
+    });
+  
+    it("should test reverseArrayItems", () => {
+      const array = [43, "what", 9, true, "cannot", false, "be", 3, true];
+      const output = [true, 3, "be", false, "cannot", true, 9, "what", 43];
+  
+      expect(reverseArrayItems(array)).toEqual(output);
+    });
+  
+    it("should test sumNumsSamePosition", () => {
+      const array1 = [4, 6, 7, 10];
+      const array2 = [8, 1, 9, 20];
+      const output = [12, 7, 16, 30];
+  
+      expect(sumNumsSamePosition(array1, array2)).toEqual(output);
+    });
+  
+    it("should test sumNums", () => {
+      const array = [4, 6, 7, 10];
+      const output = 27;
+  
+      expect(sumNums(array)).toEqual(output);
+    });
+  
+    it("should test filterArrayWithStrings", () => {
+      const array = [
+        "error01",
+        "in a galaxy",
+        "start-error",
+        "far far",
+        "away",
+        "0994-error"
+      ];
+      const output = ["in a galaxy", "far far", "away"];
+  
+      expect(filterArrayWithStrings(array)).toEqual(output);
+    });
+  
+    it("should test factorial 3", () => {
+      expect(factorial(3)).toEqual(6);
+    });
+  
+    it("should test factorial 5", () => {
+      expect(factorial(5)).toEqual(120);
+    });
+  });
